2f14ec2e312ecf35386468e5189739eb09df5733
mengeroshi <dpllgr@hotmail.com>
2023-09-28 23:32:38 -0600

    Instructions modal created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.scss';
+import Instructions from './components/organisms/Instructions/Instructions';
 
 function App() {
+  const [isInstructionsModalOpen, setIsInstructionsModalOpen] = useState(true);
+  const [isNightModeTurnedOn, setIsNightModeTurnedOn] = useState(true);
   return (
-    <div className={styles.App}>
-      App
+    <div className={`${styles.App} ${isNightModeTurnedOn ? 'NIGHT' : 'DAY'}`}>
+      <Instructions
+        isOpen={isInstructionsModalOpen}
+        onClose={() => { setIsInstructionsModalOpen(false); }}
+        isNightModeTurnedOn={isNightModeTurnedOn}
+      />
     </div>
   );
 }
---
+++ src/components/organisms/Instructions/Instructions.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Modal from '../../atoms/Modal/Modal';
+import Word from '../../molecules/Word/Word';
+import styles from './Instructions.module.scss';
+
+export default function Instructions(
+  {
+    isOpen,
+    onClose,
+    isNightModeTurnedOn,
+  }: {
+    isOpen: boolean;
+    onClose: () => void;
+    isNightModeTurnedOn: boolean
+  },
+) {
+  return (
+    <Modal isOpen={isOpen}>
+      <div className={`${styles.main} ${isNightModeTurnedOn ? 'NIGHT' : 'DAY'}`}>
+        <h1>Como jugar</h1>
+        <p className={styles.firstPg}>
+          Adivina la palabra oculta en cinco intentos.
+        </p>
+        <p>Cada intento debe ser una palabra válida de 5 letras.</p>
+        <p>
+          Después de cada intento el color de las letras cambia para mostrar
+          qué tan cerca estás de acertar la palabra.
+
+        </p>
+        <h2>Ejemplos</h2>
+        <div className={styles.wordDesc}>
+          <Word word="gatos" wordToDiscover="gxxxx" />
+          <p>
+            La letra
+            <span> G </span>
+            está en la palabra y en la posición correcta.
+          </p>
+        </div>
+        <div className={styles.wordDesc}>
+          <Word word="vocal" wordToDiscover="xxxxc" />
+          <p>
+            La letra
+            <span> C </span>
+            está en la palabra pero en la posición incorrecta.
+          </p>
+        </div>
+        <div className={styles.wordDesc}>
+          <Word word="canto" wordToDiscover="xxxxx" />
+          <p>
+            La letra
+            <span> O </span>
+            no está en la palabra.
+          </p>
+        </div>
+        <p className={styles.penultimate}>
+          Puede haber letras repetidas.
+          Las pistas son independientes para cada letra.
+        </p>
+        <p className={styles.last}>¡Una palabra nueva cada 5 minutos!</p>
+        <button type="button" onClick={onClose}>!JUGAR¡</button>
+      </div>
+    </Modal>
+  );
+}
