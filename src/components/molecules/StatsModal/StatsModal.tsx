4f17795166dec1fc5f3a740462c8d7950a2c72e2
mengeroshi <dpllgr@hotmail.com>
2023-09-30 09:02:10 -0600

    timer implemented

--- src/components/molecules/StatsModal/StatsModal.tsx
+++ src/components/molecules/StatsModal/StatsModal.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useMemo } from 'react';
+import { useTimer } from 'react-timer-hook';
 import Modal from '../../atoms/Modal/Modal';
 import styles from './StatsModal.module.scss';
 import { useGetWordleContext } from '../../../context';
+import { onStartGame } from '../../../utils';
 
 export default function StatsModal() {
   const {
-    wordleState: { isDayThemeTurnedOn, isStatsModalOpen },
-    modifiers: { togleStatsModal },
+    wordleState: {
+      isDayThemeTurnedOn, isStatsModalOpen, stats, game: { alreadyPlayedWords, started },
+    },
+    modifiers: { togleStatsModal, startGame },
   } = useGetWordleContext();
 
+  const timeStamp = useMemo(() => new Date(Date.now().valueOf() + 300000), [isStatsModalOpen]);
+  // eslint-disable-next-line no-unused-vars
+  const {
+    seconds,
+    minutes,
+  } = useTimer({ expiryTimestamp: timeStamp });
+
   const onClickButton = () => {
-    togleStatsModal();
+    if (started) {
+      togleStatsModal();
+    }
+    if (((minutes && seconds) === 0) && !started) {
+      onStartGame(startGame, alreadyPlayedWords);
+      togleStatsModal();
+    }
   };
   return (
     <Modal isOpen={isStatsModalOpen}>
@@ -18,26 +36,36 @@ export default function StatsModal() {
         <h3>Estadisticas</h3>
         <div className={styles.row}>
           <div className={styles.column}>
-            <h3>8</h3>
+            <h3>{stats.length}</h3>
             <span>Jugadas</span>
           </div>
           <div className={styles.column}>
-            <h3>8</h3>
+            <h3>{stats.filter((stat) => stat === 'WIN').length}</h3>
             <span>Victorias</span>
           </div>
         </div>
         <div className={styles.column}>
-          <p>
-            La palabra era
-            {' '}
-            <span>PERRO</span>
-          </p>
+          {
+            stats[stats.length - 1] === 'LOOSE' ? (
+              <p>
+                La palabra era
+                {' '}
+                <span>{alreadyPlayedWords}</span>
+              </p>
+            ) : null
+          }
 
         </div>
-        <div className={styles.column}>
-          <p>SIGUIENTE</p>
-          <h4>4:10</h4>
-        </div>
+        {started ? null : (
+          <div className={styles.column}>
+            <p>SIGUIENTE</p>
+            <h4>
+              {minutes}
+              :
+              {seconds}
+            </h4>
+          </div>
+        )}
         <button type="button" onClick={onClickButton}>
           Aceptar
         </button>
